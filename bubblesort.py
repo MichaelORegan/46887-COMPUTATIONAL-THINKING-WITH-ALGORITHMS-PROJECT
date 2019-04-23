8af0f952cd2a8c1b82f231ce13bff018b3a7935e
MichaelORegan <g00364738@gmit.ie>
2019-04-23 18:04:25 +0100

    commit with 3 of the sorting algorithms chosen, these are from www.interactivepython.org

---
+++ bubblesort.py
@@ -0,0 +1,14 @@
+# Michael O'Regan 09/April/2019
+# http://interactivepython.org/courselib/static/pythonds/SortSearch/TheMergeSort.html
+
+def bubbleSort(alist):
+    for passnum in range(len(alist)-1,0,-1):
+        for i in range(passnum):
+            if alist[i]>alist[i+1]:
+                temp = alist[i]
+                alist[i] = alist[i+1]
+                alist[i+1] = temp
+
+alist = [54,26,93,17,77,31,44,55,20]
+bubbleSort(alist)
+print(alist)
